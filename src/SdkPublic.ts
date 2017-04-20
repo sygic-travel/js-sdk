8e09952d060132e10a06e676a4488935eb5cc5a5
Radovan Fekeč <radovan.fekec@sygictravel.com>
2017-04-20 11:42:48 +0200

    Update typescript, remove dead code

--- src/SdkPublic.ts
+++
@@ -1,8 +0,0 @@
-import { dummyFunction } from './Places';
-import { SdkBase } from './SdkBase';
-
-export default class SdkPublic extends SdkBase {
-	public dummyFunction() {
-		return dummyFunction();
-	}
-}
