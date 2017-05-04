ff39f8c5a987320a6142f461b043d6fce6dc5c6f
Radovan Fekeč <radovan.fekec@sygictravel.com>
2017-05-04 10:34:05 +0200

    Implement setUserSession with api key and access token

--- src/StSDK.ts
+++ src/StSDK.ts
@@ -2,10 +2,15 @@ import { BaseSDK } from './BaseSDK';
 import { Bounds } from './Geo';
 import { Medium } from './Media';
 import { getPlaceDetailed, getPlaceMedia, getPlaces, Place, PlacesFilter, PlacesFilterJSON } from './Places';
+import { setUserSession } from './Settings/index';
 import { CanvasSize, spread, SpreadResult, SpreadSizeConfig } from './Spread';
 import { getTripDetailed, getTrips, Trip } from './Trip';
 
 export default class StSDK extends BaseSDK {
+	public setUserSession(key: string | null, token: string | null): void {
+		return setUserSession(key, token);
+	}
+
 	public getPlaces(filter: PlacesFilterJSON): Promise<Place[]> {
 		return getPlaces(new PlacesFilter(filter));
 	}
--- src/Xhr/Xhr.ts
+++ src/Xhr/Xhr.ts
@@ -1,5 +1,5 @@
 import axios, {AxiosInstance} from 'axios';
-import { getApiUrl, getClientKey } from '../Settings';
+import { getAccessToken, getApiKey, getApiUrl, getClientKey } from '../Settings';
 import { ApiResponse } from './ApiResponse';
 
 export const axiosInstance: AxiosInstance = axios.create();
@@ -8,20 +8,13 @@ axiosInstance.interceptors.request.use((config) => {
 	if (!config.baseURL) {
 		throw new Error('API Url not set');
 	}
-
-	if (!config.headers['x-api-key']) {
-		throw new Error('Client key not set');
-	}
-
 	return config;
 });
 
 export async function get(url: string): Promise<ApiResponse> {
 	const response = await axiosInstance.get(url, {
-		baseURL: getApiUrl(),
-		headers: {
-			'x-api-key': getClientKey()
-		}
+		baseURL: buildBaseUrl(),
+		headers: buildHeaders()
 	});
 
 	return new ApiResponse(
@@ -30,4 +23,30 @@ export async function get(url: string): Promise<ApiResponse> {
 		response.data.status_message,
 		response.data.data
 	);
+}
+
+function buildBaseUrl(): string {
+	let baseUrl: string = getApiUrl();
+	const apiKey: string | null = getApiKey();
+	if (apiKey) {
+		baseUrl = baseUrl + apiKey;
+	}
+	return baseUrl;
+}
+
+function buildHeaders() {
+	const accessToken = getAccessToken();
+	const headers = {};
+
+	const clientKey = getClientKey();
+
+	if (clientKey) {
+		headers['x-api-key'] = clientKey;
+	}
+
+	if (accessToken) {
+		headers['Authorization'] = 'Bearer ' + accessToken;
+	}
+
+	return headers;
 }
