8e247ef55527b2b44d25d1cefbf37b0d61c6e6b8
Rostislav Wolny <rostislav.wolny@tripomatic.com>
2017-05-18 15:47:48 +0200

    trip/mapper: mapTripToApiResponse  added

--- src/Trip/Mapper.ts
+++ src/Trip/Mapper.ts
@@ -1,4 +1,4 @@
-import { camelizeKeys } from 'humps';
+import { camelizeKeys, decamelizeKeys } from 'humps';
 
 import { Place } from '../Places';
 import { Day, ItineraryItem, Trip, TripMedia, TripPrivileges } from './Trip';
@@ -21,13 +21,13 @@ export const mapTrip = (trip, days: Day[] | null): Trip => {
 		name: trip.name,
 		version: trip.version,
 		startsOn: trip.starts_on,
+		endsOn: trip.ends_on,
 		updatedAt: trip.updated_at,
 		isDeleted: trip.is_deleted,
-		endsOn: trip.ends_on,
 		url: trip.url,
-		days,
 		media: camelizeKeys(trip.media) as TripMedia,
-		privileges: camelizeKeys(trip.privileges) as TripPrivileges
+		privileges: camelizeKeys(trip.privileges) as TripPrivileges,
+		days
 	} as Trip;
 };
 
@@ -63,3 +63,39 @@ export function putPlacesToTrip(trip: Trip, places: Place[]): Trip {
 	}
 	return trip;
 }
+
+export const mapTripToApiResponse = (trip: Trip): object => {
+	return {
+		id: trip.id,
+		owner_id: trip.ownerId,
+		privacy_level: trip.privacyLevel,
+		name: trip.name,
+		version: trip.version,
+		starts_on: trip.startsOn,
+		ends_on: trip.endsOn,
+		updated_at: trip.updatedAt,
+		is_deleted: trip.isDeleted,
+		url: trip.url,
+		media: decamelizeKeys(trip.media),
+		privileges: decamelizeKeys(trip.privileges),
+		days: trip.days && trip.days.map((day: Day) => ({
+				note: day.note,
+				itinerary: day.itinerary.map((itineraryItem: ItineraryItem) => ({
+					place_id: itineraryItem.placeId,
+					start_time: itineraryItem.startTime,
+					duration: itineraryItem.duration,
+					note: itineraryItem.note,
+					transport_from_previous: itineraryItem.transportFromPrevious ? {
+						mode: itineraryItem.transportFromPrevious.mode,
+						type: itineraryItem.transportFromPrevious.type,
+						avoid: itineraryItem.transportFromPrevious.avoid,
+						start_time: itineraryItem.transportFromPrevious.startTime,
+						duration: itineraryItem.transportFromPrevious.duration,
+						note: itineraryItem.transportFromPrevious.note,
+						waypoints: itineraryItem.transportFromPrevious.waypoints
+					} : null
+				}))
+			})
+		)
+	};
+};
