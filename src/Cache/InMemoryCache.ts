f5083e9074bc14a289c01a3e869a9635bbd3c458
Radovan Fekeč <radovan.fekec@sygictravel.com>
2017-05-19 10:20:54 +0200

    Update in memory cache to return cloned objects

--- src/Cache/InMemoryCache.ts
+++ src/Cache/InMemoryCache.ts
@@ -3,18 +3,21 @@ import { ICache } from './ICache';
 export class InMemoryCache implements ICache {
 	private cache = {};
 
-	public set(key: string, value: any): void {
+	public set(key: string, value: any): any {
 		this.cache[key] = value;
+		return value !== null && typeof value === 'object' ? Object.assign({}, value) : value;
 	}
 
 	public get(key: string): any {
-		return this.cache[key];
+		const value = this.cache[key];
+		if (!value) {
+			return null;
+		}
+		return typeof value === 'object' ? Object.assign({}, value) : value;
 	}
 
 	public getBatch(keys: string[]): any[] {
-		return keys.map((key: string) => {
-			return this.cache[key];
-		});
+		return keys.map((key: string) => this.get(key));
 	}
 
 	public remove(key: string): void {
