ee165260e44a8d0e65d30a4374b71416e7e157df
Rostislav Wolny <rostislav.wolny@tripomatic.com>
2017-05-30 14:56:51 +0200

    spread: Config generating added

--- src/Spread/Config.ts
+++ src/Spread/Config.ts
@@ -1,7 +1,50 @@
+import { Bounds, getZoomFromBounds } from '../Geo';
+import { CanvasSize } from './Canvas';
+
 export interface SpreadSizeConfig {
 	radius: number;
 	margin: number;
 	name: string;
 	photoRequired?: boolean;
 	minimalRating?: number;
+}
+
+/**
+ * https://confluence.sygic.com/display/STV/Map+Markers
+ */
+export function getConfig(
+	bounds: Bounds,
+	canvas: CanvasSize
+): SpreadSizeConfig[] {
+	const zoom = getZoomFromBounds(bounds, canvas.width);
+	return [
+		{
+			radius: 29,
+			margin: 10,
+			name: 'popular',
+			photoRequired: true,
+			minimalRating: 0.3,
+		},
+		{
+			radius: 22,
+			margin: 5,
+			name: 'big',
+			photoRequired: true,
+			minimalRating: zoom < 10 ? 0.001 : 0,
+		},
+		{
+			radius: 10,
+			margin: 5,
+			name: 'medium',
+			photoRequired: false,
+			minimalRating: zoom < 10 ? 0.001 : 0,
+		},
+		{
+			radius: 4,
+			margin: 5,
+			name: 'small',
+			photoRequired: false,
+			minimalRating: zoom < 10 ? 0.001 : 0,
+		}
+	];
 }
