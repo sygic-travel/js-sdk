7692cc3ad39e8a563bcaaa7b72d742cfb9132c9d
Rostislav Wolny <rostislav.wolny@tripomatic.com>
2017-06-15 11:32:06 +0200

    user: Reset user cache when setting session

--- src/User/index.ts
+++ src/User/index.ts
@@ -1,3 +1,5 @@
+import { userCache } from '../Cache';
+import * as Settings from '../Settings';
 import * as Dao from './DataAccess';
 import { UserSettings } from './User';
 
@@ -9,4 +11,9 @@ export async function getUserSettings(): Promise<UserSettings> {
 
 export async function updateUserSettings(settings: UserSettings): Promise<UserSettings> {
 	return await Dao.updateUserSettings(settings);
+}
+
+export async function setUserSession(key: string | null, token: string | null): Promise<void> {
+	await userCache.reset();
+	return Settings.setUserSession(key, token);
 }
