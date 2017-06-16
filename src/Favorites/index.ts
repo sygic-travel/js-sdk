dda6a084c5250da45cccd9322e36070457ca646a
Rostislav Wolny <rostislav.wolny@tripomatic.com>
2017-06-16 12:27:36 +0200

    favorites: api refatored to dataAccess with cache

--- src/Favorites/index.ts
+++ src/Favorites/index.ts
@@ -1,19 +1,12 @@
 import { Location } from '../Geo';
-import { ApiResponse } from '../Xhr/ApiResponse';
-import * as Api from './Api';
+import * as Dao from './DataAccess';
 
 export async function getFavoritesIds(): Promise<string[]> {
-	const apiResponse: ApiResponse = await Api.getFavorites();
-	if (!apiResponse.data.hasOwnProperty('favorites')) {
-		throw new Error('Wrong API response');
-	}
-	return apiResponse.data.favorites.map((favorite) => {
-		return favorite.place_id;
-	});
+	return Dao.getFavorites();
 }
 
 export async function addPlaceToFavorites(id: string): Promise<void> {
-	await Api.addPlaceToFavorites(id);
+	await Dao.addPlaceToFavorites(id);
 }
 
 export async function addCustomPlaceToFavorites(
@@ -21,10 +14,9 @@ export async function addCustomPlaceToFavorites(
 	location: Location,
 	address: string
 ): Promise<string> {
-	const apiResponse: ApiResponse = await Api.addCustomPlaceToFavorites(name, location, address);
-	return apiResponse.data.favorite.place_id;
+	return await Dao.addCustomPlaceToFavorites(name, location, address);
 }
 
 export async function removePlaceFromFavorites(id: string): Promise<void> {
-	await Api.removePlaceFromFavorites(id);
+	await Dao.removePlaceFromFavorites(id);
 }
