08a1ae61a3815837eaaade0c2aeee8fc102fc61e
Rostislav Wolny <rostislav.wolny@tripomatic.com>
2017-06-29 14:58:10 +0200

    spreadPlacesOnMapV2: experimental implementation

---
+++ src/SpreadV2/Config.ts
@@ -0,0 +1,104 @@
+import { camelize } from 'humps';
+
+export interface CategoriesCoefficients {
+	noCategory: number;
+	discovering: number;
+	eating: number;
+	goingOut: number;
+	hiking: number;
+	playing: number;
+	relaxing: number;
+	shopping: number;
+	sightseeing: number;
+	sleeping: number;
+	doingSports: number;
+	traveling: number;
+}
+
+export interface SpreadSizeConfig {
+	radius: number;
+	margin: number;
+	name: string;
+	photoRequired: boolean;
+	zoomLevelLimits: number[];
+	disabledCategories: string[];
+}
+
+/**
+ * https://confluence.sygic.com/display/STV/Map+Markers+2
+ */
+export function getConfig(): SpreadSizeConfig[] {
+	return [
+		{
+			radius: 29,
+			margin: 10,
+			name: 'popular',
+			photoRequired: true,
+			zoomLevelLimits: [1, 1, 1, 1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1],
+			disabledCategories: [
+				'no_category', 'eating', 'going_out', 'playing', 'shopping', 'sleeping', 'doing_sports', 'traveling'
+			]
+		},
+		{
+			radius: 22,
+			margin: 5,
+			name: 'big',
+			photoRequired: true,
+			zoomLevelLimits: [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.07, 0.05, 0.04, 0.03, 0.03, 0.03, 0.03, 0.03, 0.03, 0.02, 0.01],
+			disabledCategories: [
+				'no_category', 'eating', 'going_out', 'playing', 'shopping', 'sleeping', 'doing_sports', 'traveling'
+			]
+		},
+		{
+			radius: 10,
+			margin: 5,
+			name: 'medium',
+			photoRequired: false,
+			zoomLevelLimits: [0.07, 0.07, 0.07, 0.07, 0.07, 0.07, 0.06, 0.04, 0.03, 0.02, 0.01, 0.01, 0.01, 0.01, 0.005, 0],
+			disabledCategories: []
+		},
+		{
+			radius: 4,
+			margin: 5,
+			name: 'medium',
+			photoRequired: false,
+			zoomLevelLimits: [0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.04, 0.03, 0.02, 0.01, 0.005, 0.005, 0.005, 0.005, 0],
+			disabledCategories: []
+		},
+	];
+}
+
+export function getRatingCoeficientFromCategories(
+	categoriesCoefficients: CategoriesCoefficients,
+	categories: string[]
+): number {
+	if (categories.length === 0) {
+		categories = ['no_category'];
+	}
+	return categories.reduce( (maxCoef, category) => {
+		if (categoriesCoefficients[camelize(category)] && categoriesCoefficients[camelize(category)] > maxCoef) {
+			return categoriesCoefficients[camelize(category)];
+		}
+		return maxCoef;
+	}, 0);
+}
+
+export function isDisabledByCategory(
+	disabledCategories: string[],
+	placeCategories: string[]
+): boolean {
+	if (placeCategories.length === 0) {
+		placeCategories = ['no_category'];
+	}
+	if (placeCategories.length > disabledCategories.length) {
+		return false;
+	}
+
+	for (const category of placeCategories) {
+		if (disabledCategories.indexOf(category) === -1) {
+			return false;
+		}
+	}
+
+	return true;
+}
---
+++ src/SpreadV2/Spreader.ts
@@ -0,0 +1,107 @@
+import { Bounds, Coordinate, getZoomFromBounds, locationToCanvasCoordinate } from '../Geo';
+import { Place } from '../Places/Place';
+import { CanvasSize } from '../Spread';
+import {
+	CategoriesCoefficients,
+	getRatingCoeficientFromCategories,
+	isDisabledByCategory,
+	SpreadSizeConfig
+} from './Config';
+
+export function spread(
+	places: Place[],
+	vipPlaces: Place[],
+	markerSizes: SpreadSizeConfig[],
+	bounds: Bounds,
+	canvas: CanvasSize,
+	categoriesCoefficients?: CategoriesCoefficients | null
+): SpreadResult {
+	const zoom = getZoomFromBounds(bounds, canvas.width);
+	const finalResult = vipPlaces.reduce((result: SpreadResult, place: Place): SpreadResult => {
+		return detectRenderSize(result, place, markerSizes, bounds, canvas, zoom);
+	}, {
+		hidden: [],
+		visible: []
+	});
+	return places.reduce((result: SpreadResult, place: Place): SpreadResult => {
+		return detectRenderSize(result, place, markerSizes, bounds, canvas, zoom, categoriesCoefficients);
+	}, finalResult);
+}
+
+const detectRenderSize = (
+	result: SpreadResult,
+	place: Place,
+	markerSizes: SpreadSizeConfig[],
+	bounds: Bounds,
+	canvas: CanvasSize,
+	zoom: number,
+	categoriesCoefficients?: CategoriesCoefficients | null
+): SpreadResult => {
+	let spreadRating = 0;
+	if (categoriesCoefficients && place.level === 'poi') {
+		spreadRating = place.rating * getRatingCoeficientFromCategories(categoriesCoefficients, place.categories);
+	} else {
+		spreadRating = place.rating;
+	}
+
+	if (!place.location) {
+		result.hidden.push(place);
+		return result;
+	}
+
+	const coordinate = locationToCanvasCoordinate(place.location, bounds, canvas);
+
+	for (const size of markerSizes) {
+		if (size.photoRequired && !place.thumbnailUrl) {
+			continue;
+		}
+
+		const minimalRating = size.zoomLevelLimits[zoom - 1] ?
+			size.zoomLevelLimits[zoom - 1] : size.zoomLevelLimits[size.zoomLevelLimits.length - 1];
+		if (minimalRating && spreadRating <= minimalRating) {
+			continue;
+		}
+
+		if (place.level === 'poi' && isDisabledByCategory(size.disabledCategories, place.categories)) {
+			continue;
+		}
+
+		if (!intersects(size, coordinate, result.visible)) {
+			result.visible.push({
+				place,
+				coordinate,
+				size
+			});
+			return result;
+		}
+	}
+	result.hidden.push(place);
+	return result;
+};
+
+const intersects = (size: SpreadSizeConfig, coordinate: Coordinate, spreadedPlaces: SpreadedPlace[]): boolean => {
+	let radius2: number;
+	let intersection: boolean;
+	const radius1 = size.radius;
+	for (const spreadedPlace of spreadedPlaces) {
+		const biggerMargin = size.margin > spreadedPlace.size.margin ? size.margin : spreadedPlace.size.margin;
+		radius2 = spreadedPlace.size.radius;
+		intersection =  (Math.pow(coordinate.x - spreadedPlace.coordinate.x, 2) +
+			Math.pow(coordinate.y - spreadedPlace.coordinate.y, 2)) <= Math.pow(radius1 + radius2 + biggerMargin, 2);
+		if (intersection) {
+			return true;
+		}
+	}
+	return false;
+};
+
+export interface SpreadResult {
+	visible: SpreadedPlace[];
+	hidden: Place[];
+}
+
+export interface SpreadedPlace {
+	place: Place;
+	coordinate: Coordinate;
+	size: SpreadSizeConfig;
+}
