9bcd2d79cc08388544dfd3d38d664e05d1b3c048
Radovan Fekeč <radovan.fekec@sygictravel.com>
2017-07-25 09:54:14 +0200

    Implement getAll method on ICache

--- src/Cache/ICache.ts
+++ src/Cache/ICache.ts
@@ -1,6 +1,7 @@
 export interface ICache {
 	set(key: string, value: any): Promise<void>;
 	get(key: string): Promise<any>;
+	getAll(): Promise<any[]>
 	getBatch(keys: string[]): Promise<any[]>;
 	getBatchMap(keys: string[]): Promise<Map<string, any>>;
 	remove(key: string): Promise<any>;
