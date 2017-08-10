2a2e276e3b0cf76b072827f5ca12fb5b2015ded0
Rostislav Wolny <rostislav.wolny@tripomatic.com>
2017-08-10 16:04:16 +0200

    routes: Updated plane speed and airpoirt delay in estimator

--- src/Route/Estimator.ts
+++ src/Route/Estimator.ts
@@ -2,13 +2,13 @@ import { encode } from 'polyline';
 import { Direction } from '.';
 import { EARTH_RADIUS, getDistance, Location } from '../Geo';
 
-const PLANE_SPEED: number = 800;
+const PLANE_SPEED: number = 900;
 
 export const estimatePlaneDirection = (origin: Location, destination: Location): Direction => {
 	const distance = getDistance(origin, destination, EARTH_RADIUS);
 	return {
 		distance,
-		duration: Math.round((distance / PLANE_SPEED) * 3.6 + 3 * 3600),
+		duration: Math.round((distance / PLANE_SPEED) * 3.6 + 3 * 40 * 60),
 		polyline: encode([[origin.lat, origin.lng], [destination.lat, destination.lng]]),
 		mode: 'plane',
 		type: 'fastest',
