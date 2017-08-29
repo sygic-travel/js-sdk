13d2885cabb23e4c00067e1652325c718227fa10
Rostislav Wolny <rostislav.wolny@tripomatic.com>
2017-08-29 16:57:02 +0200

    hotels: Filter class and test added

---
+++ src/Hotels/Filter.test.ts
@@ -0,0 +1,113 @@
+import * as chai from 'chai';
+
+import { HotelsFilter, HotelsFilterJSON } from '.';
+
+describe('HotelsFilter', () => {
+	describe('#filterToQueryString', () => {
+		it('should build query string from basic paramateres', () => {
+			const filterJSON: HotelsFilterJSON = {
+				adults: 3,
+				checkIn: '2017-11-11',
+				checkOut: '2017-11-12',
+				bounds: {
+					south: 1,
+					west: 2,
+					north: 3,
+					east: 4,
+				}
+			};
+
+			const filter = new HotelsFilter(filterJSON);
+			const expectedQuery = 'adults=3&bounds=1%2C2%2C3%2C4&check_in=2017-11-11&check_out=2017-11-12';
+
+			chai.expect(filter.toQueryString()).to.equal(expectedQuery);
+		});
+
+		it('should build query string from all paramateres but not zoom', () => {
+			const filterJSON: HotelsFilterJSON = {
+				adults: 3,
+				children: [1, 2],
+				checkIn: '2017-11-11',
+				checkOut: '2017-11-12',
+				mapTileBounds: ['123123', '012012'],
+				limit: 10,
+				zoom: 5,
+				maxPrice: 40,
+				minPrice: 20,
+				currency: 'CZK',
+				stars: [1, 5],
+				hotelFacilities: ['x', 'y'],
+				roomFacilities: ['a', 'b'],
+				propertyTypes: ['c', 'd'],
+				minReviewScore: 10
+			};
+
+			const filter = new HotelsFilter(filterJSON);
+			const expectedQuery = 'adults=3&check_in=2017-11-11&check_out=2017-11-12&children=1%2C2&currency=CZK' +
+				'&hotel_facilities=x%2Cy&limit=10&map_tile_bounds=123123%2C012012&max_price=40&min_price=20&min_review_score=10' +
+				'&property_types=c%7Cd&room_facilities=a%2Cb&stars=1%7C5';
+
+			chai.expect(filter.toQueryString()).to.equal(expectedQuery);
+		});
+	});
+
+	describe('#validate', () => {
+		it('should raise error on missing adults', () => {
+			const createFilter = () => {
+				return new HotelsFilter({
+					adults: 0,
+					checkIn: '2017-11-11',
+					checkOut: '2017-11-12',
+				});
+			};
+			chai.expect(createFilter).to.throw('Adults count is mandatory.');
+		});
+		it('should raise error when bounds passed witn mapTileBounds', () => {
+			const createFilter = () => {
+				return new HotelsFilter({
+					adults: 1,
+					checkIn: '2017-11-11',
+					checkOut: '2017-11-12',
+					bounds: {
+						south: 1,
+						west: 2,
+						north: 3,
+						east: 4,
+					},
+					mapTileBounds: ['1', '2']
+				});
+			};
+			chai.expect(createFilter).to.throw('Bounds and mapTileBounds have to be used exclusively.');
+		});
+		it('should raise error when same dates are passed', () => {
+			const createFilter = () => {
+				return new HotelsFilter({
+					adults: 1,
+					checkIn: '2017-11-11',
+					checkOut: '2017-11-11',
+				});
+			};
+			chai.expect(createFilter).to.throw('Invalid checkIn/checkOut combination.');
+		});
+		it('should raise error when some invalid dates are passed', () => {
+			const createFilter = () => {
+				return new HotelsFilter({
+					adults: 1,
+					checkIn: 'xxx',
+					checkOut: 'fdfdfd',
+				});
+			};
+			chai.expect(createFilter).to.throw('Invalid checkIn date.');
+		});
+		it('should raise error when some invalid dates are passed', () => {
+			const createFilter = () => {
+				return new HotelsFilter({
+					adults: 1,
+					checkIn: '2017-11-11',
+					checkOut: 'fdfdfd',
+				});
+			};
+			chai.expect(createFilter).to.throw('Invalid checkOut date.');
+		});
+	});
+});
---
+++ src/Hotels/Filter.ts
@@ -0,0 +1,167 @@
+import { stringify } from 'query-string';
+
+import { Bounds, locationToMapTileKey } from '../Geo';
+
+export interface HotelsFilterJSON {
+	checkIn: string;
+	checkOut: string;
+	adults: number;
+	children?: number[] | null;
+	maxPrice?: number | null;
+	minPrice?: number | null;
+	minReviewScore?: number | null;
+	bounds?: Bounds | null;
+	mapTileBounds?: string[] | null;
+	stars?: number[] | null;
+	currency?: string | null;
+	propertyTypes?: string[] | null;
+	hotelFacilities?: string[] | null;
+	roomFacilities?: string[] | null;
+	limit?: number;
+	zoom?: number;
+}
+
+export interface HotelsFilterQuery {
+	check_in: string;
+	check_out: string;
+	adults: number;
+	children?: string;
+	max_price?: number;
+	min_price?: number;
+	min_review_score?: number;
+	bounds?: string;
+	map_tile_bounds?: string;
+	stars?: string;
+	currency?: string;
+	property_types?: string;
+	hotel_facilities?: string;
+	room_facilities?: string;
+	limit?: number;
+	zoom?: number;
+}
+
+export class HotelsFilter {
+	protected _checkIn: string;
+	protected _checkOut: string;
+	protected _adults: number;
+	protected _children?: number[] | null;
+	protected _maxPrice?: number | null;
+	protected _minPrice?: number | null;
+	protected _minReviewScore?: number | null;
+	private _bounds?: Bounds | null;
+	protected _mapTileBounds?: string[] | null;
+	protected _stars?: number[] | null;
+	protected _currency?: string | null;
+	protected _propertyTypes?: string[] | null;
+	protected _hotelFacilities?: string[] | null;
+	protected _roomFacilities?: string[] | null;
+	protected _limit?: number;
+	private _zoom?: number;
+
+	constructor(filter: HotelsFilterJSON) {
+		this._checkIn = filter.checkIn;
+		this._checkOut = filter.checkOut;
+		this._adults = filter.adults;
+		this._children = filter.children;
+		this._maxPrice = filter.maxPrice;
+		this._minPrice = filter.minPrice;
+		this._minReviewScore = filter.minReviewScore;
+		this._bounds = filter.bounds;
+		this._mapTileBounds = filter.mapTileBounds;
+		this._stars = filter.stars;
+		this._currency = filter.currency;
+		this._propertyTypes = filter.propertyTypes;
+		this._hotelFacilities = filter.hotelFacilities;
+		this._roomFacilities = filter.roomFacilities;
+		this._limit = filter.limit;
+		this._zoom = filter.zoom;
+		this.validate();
+	}
+
+	get zoom(): number | null {
+		return this._zoom ? this._zoom : null;
+	}
+
+	get bounds(): Bounds | null {
+		return this._bounds ? this._bounds : null;
+	}
+
+	public toQueryObject(): HotelsFilterQuery {
+		const query: HotelsFilterQuery = {
+			check_in: this._checkIn,
+			check_out: this._checkOut,
+			adults: this._adults,
+		};
+		if (this._children) {
+			query.children = this._children.join(',');
+		}
+		if (this._maxPrice) {
+			query.max_price = this._maxPrice;
+		}
+		if (this._minPrice) {
+			query.min_price = this._minPrice;
+		}
+		if (this._minReviewScore) {
+			query.min_review_score = this._minReviewScore;
+		}
+		if (this._bounds) {
+			query.bounds = this._bounds.south + ',' + this._bounds.west + ',' + this._bounds.north + ',' + this._bounds.east;
+		}
+		if (this._mapTileBounds) {
+			query.map_tile_bounds = this._mapTileBounds.join(',');
+		}
+		if (this._stars) {
+			query.stars = this._stars.join('|');
+		}
+		if (this._currency) {
+			query.currency = this._currency;
+		}
+		if (this._propertyTypes) {
+			query.property_types = this._propertyTypes.join('|');
+		}
+		if (this._hotelFacilities) {
+			query.hotel_facilities = this._hotelFacilities.join(',');
+		}
+		if (this._roomFacilities) {
+			query.room_facilities = this._roomFacilities.join(',');
+		}
+		if (this._limit) {
+			query.limit = this._limit;
+		}
+		return query;
+	}
+
+	public toQueryString(): string {
+		return stringify(this.toQueryObject());
+	}
+
+	public switchBoundsToMapTileBounds(): HotelsFilter {
+		const that = Object.create(this);
+		if (!this._bounds || !this._zoom) {
+			return Object.assign(that, this, {_mapTileBounds: null});
+		}
+		const swMapTile = locationToMapTileKey({lat: this._bounds.south, lng: this._bounds.west}, this._zoom);
+		const neMapTile = locationToMapTileKey({lat: this._bounds.north, lng: this._bounds.east}, this._zoom);
+		return Object.assign(that, this, {_bounds: null, _mapTileBounds: [swMapTile, neMapTile]});
+	}
+
+	private validate(): void {
+		if (this._bounds && this._mapTileBounds) {
+			throw new Error('Bounds and mapTileBounds have to be used exclusively.');
+		}
+		const chInDate = new Date(this._checkIn);
+		const chOutDate = new Date(this._checkOut);
+		if (chInDate.toString() === 'Invalid Date') {
+			throw new Error('Invalid checkIn date.');
+		}
+		if (chOutDate.toString() === 'Invalid Date') {
+			throw new Error('Invalid checkOut date.');
+		}
+		if (chOutDate <= chInDate) {
+			throw new Error('Invalid checkIn/checkOut combination.');
+		}
+		if (!this._adults) {
+			throw new Error('Adults count is mandatory.');
+		}
+	}
+}
