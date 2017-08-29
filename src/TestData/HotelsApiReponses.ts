289cf646b363721d2fd6679b116accff861dc319
Rostislav Wolny <rostislav.wolny@tripomatic.com>
2017-08-29 16:58:26 +0200

    hotels: DataAccessTest and test data added

---
+++ src/TestData/HotelsApiReponses.ts
@@ -0,0 +1,38 @@
+/* tslint:disable */
+export const hotels = {
+	"hotels": [
+		{
+			"place": {
+				"id": "hotel:12345",
+				"level": "poi",
+				"rating": 10.004,
+				"quadkey": "120212302033311321",
+				"location": {
+					"lat": 50.0866003,
+					"lng": 14.4105518
+				},
+				"bounding_box": null,
+				"name": "Charles Bridge",
+				"name_suffix": "Prague, Czech Republic",
+				"url": "https://alpha.travel.sygic.com/go/poi:345430",
+				"marker": "sightseeing:art:artwork",
+				"categories": [
+					"sightseeing"
+				],
+				"parent_ids": [
+					"city:5",
+					"region:26009",
+					"region:26011",
+					"country:5",
+					"continent:1"
+				],
+				"perex": "This 14-century bridge connects the Old Town with Lesser Town and the Prague Castle. With its length of 621 meters, solid Gothic stone…",
+				"thumbnail_url": "https://alpha-media-cdn.sygictraveldata.com/media/poi:345430"
+			},
+			"booking_com": {
+				"hotel_id": "1201343",
+				"price": 169.47
+			}
+		}
+	]
+};
