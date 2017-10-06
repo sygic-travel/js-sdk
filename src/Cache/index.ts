bc4638b230fdb6b344b750d1a84f63aa759bafd2
Rostislav Wolny <rostislav.wolny@tripomatic.com>
2017-10-06 16:58:53 +0200

    cache: Cookie cache added

--- src/Cache/index.ts
+++ src/Cache/index.ts
@@ -1,3 +1,4 @@
+import { BrowserCookieCache } from './BrowserCookieCache';
 import { ICache } from './ICache';
 import { InMemoryCache } from './InMemoryCache';
 
@@ -6,3 +7,5 @@ export const tripsDetailedCache: ICache = new InMemoryCache();
 export const routesCache: ICache = new InMemoryCache();
 export const userCache: ICache = new InMemoryCache();
 export const favoritesCache: ICache = new InMemoryCache();
+export const sessionCache: ICache = (typeof window === 'object' && window.document)
+	? new BrowserCookieCache() : new InMemoryCache();
