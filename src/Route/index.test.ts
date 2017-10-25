fac8ce4ddde4803d59615ae8bde99a2c12cdafd8
Rostislav Wolny <rostislav.wolny@tripomatic.com>
2017-10-25 17:15:50 +0200

    routes: Added test for index.ts

---
+++ src/Route/index.test.ts
@@ -0,0 +1,52 @@
+import * as chai from 'chai';
+import * as cloneDeep from 'lodash.clonedeep';
+import * as sinon from 'sinon';
+import { SinonSandbox } from 'sinon';
+
+import * as RoutesController from '.';
+import { Route, TripDayRoutes } from '.';
+import { Dao as placesDao, Place } from '../Places';
+import { route } from '../TestData/DirectionsEntities';
+import { tripDetailed } from '../TestData/TripExpectedResults';
+import { Dao as tripDao, Trip } from '../Trip';
+import * as routesDao from './DataAccess';
+
+let sandbox: SinonSandbox;
+
+describe('RoutesController', () => {
+
+	beforeEach(() => {
+		sandbox = sinon.sandbox.create();
+	});
+
+	afterEach(() => {
+		sandbox.restore();
+	});
+
+	describe('#getRoutesForTripDay', () => {
+		it('should build explicit flags correctly', async () => {
+			const trip = cloneDeep(tripDetailed);
+			trip.days[0].itinerary[1].transportFromPrevious = {
+				type: 'fastest',
+				duration: null,
+				mode: 'pedestrian',
+				avoid: [],
+				note: null,
+			};
+			trip.days[0].itinerary.push(cloneDeep(trip.days[0].itinerary[0]));
+			sandbox.stub(tripDao, 'getTripDetailed').returns(new Promise<Trip>((resolve) => {resolve(trip); }));
+			sandbox.stub(placesDao, 'getPlacesFromTripDay').returns(new Promise<Place[]>((resolve) => {resolve([
+				trip.days[0].itinerary[0].place,
+				trip.days[0].itinerary[1].place,
+				trip.days[0].itinerary[2].place
+			]); }));
+			sandbox.stub(routesDao, 'getRoutes').returns(new Promise<Route[]>((resolve) => {resolve([
+					cloneDeep(route),
+					cloneDeep(route),
+			]); }));
+			const dayRoutes: TripDayRoutes = await RoutesController.getRoutesForTripDay('12345', 0);
+			chai.expect(dayRoutes.routes.length).to.equal(2);
+			chai.expect(dayRoutes.isExplicitFlags).to.deep.equal([true, false]);
+		});
+	});
+});
