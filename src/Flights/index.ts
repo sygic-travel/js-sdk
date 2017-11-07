22d35e040f749b2a5d2815204d9e4a2045b02f24
Rostislav Wolny <rostislav.wolny@tripomatic.com>
2017-11-07 10:55:20 +0100

    flights: Module added

---
+++ src/Flights/index.ts
@@ -0,0 +1,19 @@
+import * as Dao from './DataAccess';
+import { Airline, Airport, Flight, FlightSearchResult, FlightsQuery, isOvernight, Route } from './Flight';
+
+export {
+	Airline,
+	Airport,
+	Flight,
+	FlightsQuery,
+	FlightSearchResult,
+	isOvernight,
+	Route,
+}
+
+export async function getFlights(query: FlightsQuery): Promise<FlightSearchResult[]> {
+	if (!query.adults) {
+		query.adults = 2;
+	}
+	return Dao.getFlights(query);
+}
