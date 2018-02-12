ddf49cfb2fefb5e3921365c4dcdc1dc4ae185f02
Radovan Fekeč <radovan.fekec@sygictravel.com>
2018-02-12 12:49:19 +0100

    Add task for building debug version of lib

---
+++ webpack.debug.config.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack');
+
+var baseConfig = {
+	entry: {
+		sdk: './src/sdk.ts'
+	},
+	target: 'web',
+	output: {
+		path: __dirname + '/dist',
+		library: 'SygicTravelSDK',
+		libraryTarget: 'umd',
+		umdNamedDefine: true,
+		filename: 'SygicTravelSDK.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				loader: 'ts-loader',
+				exclude: /node_modules/
+			},
+		]
+	},
+	resolve: {
+		extensions: [".ts", ".js"]
+	},
+	plugins: [
+		new webpack.LoaderOptionsPlugin({
+			minimize: false,
+			debug: true
+		})
+	]
+};
+
+module.exports = baseConfig;
