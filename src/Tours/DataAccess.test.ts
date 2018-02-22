d8e5feeea566f0809b06ef36975505ac2fe14e0d
Rostislav Wolny <rostislav.wolny@tripomatic.com>
2018-02-22 10:56:02 +0100

    tours: GYG Data access test

--- src/Tours/DataAccess.test.ts
+++ src/Tours/DataAccess.test.ts
@@ -1,14 +1,17 @@
 import * as chai from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
-import { cloneDeep } from '../Util';
-import { sandbox as sinonSandbox, SinonSandbox } from 'sinon';
+import { sandbox as sinonSandbox, SinonSandbox, SinonStub } from 'sinon';
 
 import { ApiResponse, StApi } from '../Api';
 import { setEnvironment } from '../Settings';
 import * as ToursApiTestData from '../TestData/ToursApiResponses';
 import * as ToursExpectedResults from '../TestData/ToursExpectedResults';
+import { cloneDeep } from '../Util';
 import * as Dao from './DataAccess';
-import { ToursQueryDirection, ToursViatorQuery, ToursViatorQuerySortBy } from './Tour';
+import {
+	ToursGetYourGuideQuery, ToursGetYourGuideQuerySortBy, ToursQueryDirection,
+	ToursViatorQuery, ToursViatorQuerySortBy
+} from './Tour';
 
 let sandbox: SinonSandbox;
 chai.use(chaiAsPromised);
@@ -27,7 +30,7 @@ describe('TripDataAccess', () => {
 		sandbox.restore();
 	});
 
-	describe('#getViatorTours', () => {
+	describe('#getToursViator', () => {
 		it('should correctly map tours response', () => {
 			sandbox.stub(StApi, 'get').returns(new Promise<ApiResponse>((resolve) => {
 				resolve(new ApiResponse(200, cloneDeep(ToursApiTestData.toursList)));
@@ -42,6 +45,49 @@ describe('TripDataAccess', () => {
 
 			return chai.expect(Dao.getToursViator(toursQuery))
 				.to.eventually.deep.equal(ToursExpectedResults.toursList);
+		});
+	});
+
+	describe('#getToursGetYourGuide', () => {
+		it('should call api with correct parameters', async () => {
+			const stub: SinonStub = sandbox.stub(StApi, 'get').returns(new Promise<ApiResponse>((resolve) => {
+				resolve(new ApiResponse(200, cloneDeep(ToursApiTestData.toursList)));
+			}));
+
+			const toursQuery: ToursGetYourGuideQuery = {
+				query: null,
+				bounds: null,
+				parentPlaceId: 'city:1',
+				page: null,
+				tags: [],
+				count: 10,
+				startDate: '2018-03-01',
+				endDate: '2018-03-02',
+				durationMin: 10,
+				durationMax: 100000,
+				sortBy: null,
+				sortDirection: null
+			};
+
+			await Dao.getToursGetYourGuide(toursQuery);
+			chai.expect(stub.getCall(0).args[0]).to.equal('tours/get-your-guide?count=10&duration_max=100000&' +
+				'duration_min=10&end_date=2018-03-02&parent_place_id=city%3A1&start_date=2018-03-01');
+
+			toursQuery.bounds = {
+				south: 1,
+				west: 1,
+				north: 2,
+				east: 2
+			};
+			toursQuery.page = 2;
+			toursQuery.tags = [1, 2, 3];
+			toursQuery.sortBy = ToursGetYourGuideQuerySortBy.PRICE;
+			toursQuery.sortDirection = ToursQueryDirection.DESC;
+			toursQuery.query = 'test';
+			await Dao.getToursGetYourGuide(toursQuery);
+			chai.expect(stub.getCall(1).args[0]).to.equal('tours/get-your-guide?bounds=1%2C1%2C2%2C2&count=10' +
+				'&duration_max=100000&duration_min=10&end_date=2018-03-02&page=2&parent_place_id=city%3A1&query=test&' +
+				'sort_by=price&sort_direction=desc&start_date=2018-03-01&tags=1%2C2%2C3');
 		});
 	});
 });
